still trying to fix calculation issue

circular_orbit.py
@@ -1,4 +1,6 @@
 # simple circular orbit using Euler-Cromer method
+# phi should probably be called phi here
+# when i say circular coordinates i really mean polar coordinates
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,15 +9,15 @@ from dataclasses import dataclass
 
 # constants
 step = 0.002  # in yrs
-iterations = 10  # test
+iterations = 2  # test
 earth_to_sun = 1.00  # in AU; in km it's 91.607
-pi = np.pi  # for simplicity
+pi = np.pi  # for simplicity; in radians
 
 
 @dataclass
 class Point:  # we're using spherical coordinates, boys
     r: float  # range [0, 1] in AU
-    theta: float  # can be [0, 2pi] -- anywhere on the unit circle
+    phi: float  # can be [0, 2pi] -- anywhere on the unit circle
 
 
 class Vector(Point):
@@ -35,34 +37,46 @@ class Planet:
 
     def update(self):  # Euler-Cromer method
         r = self.pos.r  # for convenience
-        theta = self.pos.theta
-        r_dot = self.vel.r
-        theta_dot = self.vel.theta
+        phi = self.pos.phi
+        r_dot = self.vel.r  # r-component of polar coordinates velocity
+        print('r_dot: {}'.format(r_dot))
+        phi_dot = self.vel.phi
         delta_t = self.delta_t
         factor = 4 * (pi ** 2) / (r ** 3)
         print("Factor: {}".format(factor))  # test
         # Cartesian position components
-        x = r * np.cos(theta)
-        y = theta * np.sin(theta)
+        x = r * np.cos(phi)
+        print("x: {}".format(x))  # test
+        y = r * np.sin(phi)
+        print('y: {}'.format(y))  # test
         # Cartesian velocity components
-        vx = r * theta_dot * np.sin(theta) + r_dot * np.cos(theta)  # x component
-        vy = r * theta_dot * np.cos(theta) + r_dot * np.sin(theta)
+        # TODO: Figure out why this line is resulting in a positive number on the first step!
+        vx = (r_dot * np.cos(phi)) - (r * phi_dot * np.sin(phi))  # x component; first term cancels on first step
+        # vx = r * np.sin(phi_dot)  # don't think this test line works
+        print('first term of vx: {}'.format(r_dot * np.cos(phi)))
+        print('second term of vx: {}'.format(r * phi_dot * np.sin(phi)))
+        print('vx: {}'.format(vx))  # test
+        vy = (r * phi_dot * np.cos(phi)) + (r_dot * np.sin(phi))  # y component; km/s
+        print('vy: {}'.format(vy))  # test
 
         # compute updated Cartesian velocity components
-        vx_new = vx - factor * x * delta_t
-        vy_new = vy - factor * y * delta_t
+        vx_new = vx - (factor * x * delta_t)
+        vy_new = vy - (factor * y * delta_t)
 
         # calculate updated Cartesian position components
-        x_new = x + vx_new * delta_t
-        y_new = y + vy_new * delta_t
+        x_new = x + (vx_new * delta_t)
+        print('x_new: {}'.format(x_new))  # test
+        y_new = y + (vy_new * delta_t)
+        print('y_new: {}'.format(y_new))  # test
 
         # update position
-        self.pos.r = (x_new ** 2 + y_new ** 2) ** (1 / 2)
-        self.pos.theta = theta + (theta_dot * delta_t)
+        self.pos.r = ((x_new ** 2) + (y_new ** 2)) ** (1 / 2)
+        # self.pos.phi = phi + (phi_dot * delta_t)  # not sure about this
+        self.pos.phi = np.arctan2(y_new, x_new)  # see Wikipedia page on polar coordinates
 
         # update velocity (still circular coordinates)
-        self.vel.r = (vx_new ** 2 + vy_new ** 2) ** (1 / 2)  # r_dot
-        # not updating self.vel.theta because it is constant and independent of x and y right now
+        self.vel.r = ((vx_new ** 2) + (vy_new ** 2)) ** (1 / 2)  # r_dot
+        # not updating self.vel.phi because it is constant and independent of x and y right now
         # TODO: Check this whole method and find equations of motion
 
         return x_new, y_new
@@ -75,16 +89,27 @@ def main():
 
     # initialize the Earth
     earth_init_pos = Point(1.0, 0)  # remember this is in circular coordinates
-    earth_init_vel = Vector(0.0, 1.0)  # no radial velocity right now; only angular
+    earth_init_vel = Vector(0.0, 2 * pi)  # no radial velocity right now; only angular in rad/s
     earth = Planet(earth_init_pos, earth_init_vel, step)
 
     xdata = []
     ydata = []
+    times = []
 
+    t = 0  # initialize time
     for i in range(iterations):
         x, y = earth.update()
         xdata.append(x)
         ydata.append(y)
+        times.append(t)
+        t += step
+
+    # test
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(7, 7))
+    ax.plot(times, xdata)
+    ax.plot(times, ydata)
+    plt.show()
 
     # now animate
     plt.style.use('dark_background')

earth_sun.py
@@ -118,14 +118,14 @@ if __name__ == '__main__':
     sun = ax.plot(sun.pos.x, sun.pos.y, 'X', markersize=30, color='yellow')
     # ax.plot(x, y, 'g-')
     plt.grid(True, lw=0.3)  # plot grid
-    plt.savefig('earth_sun.png')
+    plt.savefig('output/earth_sun.png')
 
     def animate(i):
         earth.set_data(r[i] * np.cos(x[i]), r[i] * np.sin(y[i]))
         return earth
 
     animation = FuncAnimation(fig, animate, frames=len(x), interval=200, repeat=True)
-    animation.save('earth_sun.gif', writer='pillow')  # mp4 doesn't work
+    animation.save('output/earth_sun.gif', writer='pillow')  # mp4 doesn't work
     plt.show()
 
 